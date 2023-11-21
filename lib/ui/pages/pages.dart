a4f81c894db77df83eb14159a03e995c692550b6
Linaeus14 <titodqrmawan123@gmail.com>
2023-11-22 00:12:31 +0800

    del: unused import

--- lib/ui/pages/pages.dart
+++ lib/ui/pages/pages.dart
@@ -1,5 +1,4 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
-import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/gestures.dart';
 import 'package:image_picker/image_picker.dart';
